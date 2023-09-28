36f7dd51560de7c0e086393e4178b8a12c2eb5c1
moonno317 <139625145+moonno317@users.noreply.github.com>
2023-09-28 20:45:51 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
+#include <sys/socket.h>awdawdawdawd
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ncurses.h>
