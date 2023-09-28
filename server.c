213867fcb25941483c4859c70436668298556abd
moonno317 <139625145+moonno317@users.noreply.github.com>
2023-09-28 20:46:29 +0700

    Update server.c

--- server.c
+++ server.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
+#include <sys/socket.h>awddddddddddddddddddddddddddddddddddddddd
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
